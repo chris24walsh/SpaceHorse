9017ba013ea346a563539dbe61cba2ee193edd22
Chris Walsh <chris24walsh@gmail.com>
2015-11-06 09:36:06 +0000

    Version 0.2.3 - Added networking, not finetuned yet but will allow in future to play   syncronously across two or more computers. Using ENet library for   networking, it is important to include this library first to prevent   redefinitions with allegro's included winsock library.

--- allegroTest/allegroTest/main.cpp
+++ allegroTest/allegroTest/main.cpp
@@ -1,3 +1,4 @@
+#include <enet\enet.h>
 #include <allegro5/allegro5.h>
 #include "allegro5/allegro_image.h"
 #include <stdio.h>
@@ -30,7 +31,7 @@ ALLEGRO_DISPLAY_MODE disp_data;
 //Screen vars
 int gridX = 0;
 int gridY = 0;
-int numberGrids = 2;
+int numberGrids = 2; //Number of grids along one side of the square map actually...:)
 int windowWidth, windowHeight, maxX, maxY, screenWidth, screenHeight;
 int backgroundWidth = 1920;
 int backgroundHeight = 1080;
@@ -66,6 +67,16 @@ int dockingHeight[maxDockingStations] = {250, 525, 190};
 bool safe = true;
 bool dispUpgradeText = false;
 
+//Network vars
+bool useServer = true;
+ENetHost * host; //This machine
+ENetPeer * peer; //Remote machine
+
+//Network Functions
+void connectServer();
+void sendData();
+int receiveData();
+
 void abort_game(const char* message)
 {
     printf("%s \n", message);
@@ -91,13 +102,6 @@ void init(void)
 	windowHeight = disp_data.height;
 	maxX = windowWidth*numberGrids;
 	maxY = windowHeight*numberGrids;
-
-	//Setup scaling
-	//float sx = float(windowWidth) / backgroundWidth;
-	//float sy = float(windowHeight) / backgroundHeight;
-	//float scale = ((sx>sy)?sx:sy); //Scale so that background fills window with least waste
-	//screenWidth = backgroundWidth * scale;
-	//screenHeight = backgroundHeight * scale;
 	
     //al_set_new_display_flags(ALLEGRO_FULLSCREEN);
     display = al_create_display(windowWidth, windowHeight);
@@ -148,6 +152,54 @@ void init(void)
 
 	//Docking off
 	canDock = dockingScr = false;
+
+	//Initialise the network library
+	if (enet_initialize () != 0)
+    {
+        fprintf (stderr, "An error occurred while initializing ENet.\n");
+        //return EXIT_FAILURE;
+    }
+    //atexit (enet_deinitialize); //deinitialise upon program exit
+
+	//Create server
+	if (useServer) {
+		ENetAddress address;
+		/* Bind the server to the default localhost.     */
+		/* A specific host address can be specified by   */
+		/* enet_address_set_host (& address, "x.x.x.x"); */
+		address.host = ENET_HOST_ANY;
+		/* Bind the server to port 1234. */
+		address.port = 1234;
+		host = enet_host_create (& address /* the address to bind the server host to */, 
+									 32      /* allow up to 32 clients and/or outgoing connections */,
+									  2      /* allow up to 2 channels to be used, 0 and 1 */,
+									  0      /* assume any amount of incoming bandwidth */,
+									  0      /* assume any amount of outgoing bandwidth */);
+		if (host == NULL)
+		{
+			fprintf (stderr, 
+					 "An error occurred while trying to create an ENet server host.\n");
+			//exit (EXIT_FAILURE);
+		}
+	}
+
+	//Create client
+	else {
+		host = enet_host_create (NULL /* create a client host */,
+					1 /* only allow 1 outgoing connection */,
+					2 /* allow up 2 channels to be used, 0 and 1 */,
+					0	/* 57600 / 8 /* 56K modem with 56 Kbps downstream bandwidth */,
+					0	/* 14400 / 8 /* 56K modem with 14 Kbps upstream bandwidth */);
+		if (host == NULL)
+		{
+			fprintf (stderr, 
+					 "An error occurred while trying to create an ENet client host.\n");
+			//exit (EXIT_FAILURE);
+		}
+
+		//Connect to the server machine
+		connectServer();
+	}
 }
  
 void shutdown(void)
@@ -158,6 +210,10 @@ void shutdown(void)
 	if (shipSprite) al_destroy_bitmap(shipSprite);	
     if (event_queue) al_destroy_event_queue(event_queue);
 	//if (path) al_destroy_path(path);
+
+	//Network shutdown
+	enet_deinitialize();
+	enet_host_destroy(host);
 }
 
 //Game functions
@@ -362,4 +418,90 @@ int main(int argc, char* argv[])
     init();
     game_loop();
     shutdown();
+}
+
+//Network functions
+//Connect to server
+void connectServer() {
+	ENetAddress address;
+	ENetEvent event;
+	//ENetPeer *peer;
+	/* Connect to some.server.net:1234. */
+	enet_address_set_host (& address, "192.168.8.102");
+	address.port = 1234;
+	/* Initiate the connection, allocating the two channels 0 and 1. */
+	peer = enet_host_connect (host, & address, 2, 0);    
+	if (peer == NULL)
+	{
+	   fprintf (stderr, 
+				"No available peers for initiating an ENet connection.\n");
+	   exit (EXIT_FAILURE);
+	}
+	/* Wait up to 5 seconds for the connection attempt to succeed. */
+	if (enet_host_service (host, & event, 7000) > 0 &&
+		event.type == ENET_EVENT_TYPE_CONNECT)
+	{
+		puts ("Connection to some.server.net:1234 succeeded.");
+		//
+	}
+	else
+	{
+		/* Either the 5 seconds are up or a disconnect event was */
+		/* received. Reset the peer in the event the 5 seconds   */
+		/* had run out without any significant event.            */
+		enet_peer_reset (peer);
+		puts ("Connection to some.server.net:1234 failed.");
+	}
+}
+
+//Sending data
+void sendData() {
+	/* Create a reliable packet of size 7 containing "packet\0" */
+	ENetPacket * packet = enet_packet_create ("packet", strlen ("packet") + 1, ENET_PACKET_FLAG_RELIABLE);
+	/* Extend the packet so and append the string "foo", so it now */
+	/* contains "packetfoo\0"                                      */
+	enet_packet_resize (packet, strlen ("packetfoo") + 1);
+	//strcpy (& packet -> data [strlen ("packet")], "foo");
+	/* Send the packet to the peer over channel id 0. */
+	/* One could also broadcast the packet by         */
+	/* enet_host_broadcast (host, 0, packet);         */
+	enet_peer_send (peer, 0, packet);
+	/* One could just use enet_host_service() instead. */
+	enet_host_flush (host);
+}
+
+//Receiving data
+int receiveData() {
+	ENetEvent event;
+	// Non-blocking event poll
+	while (enet_host_service (host, & event, 0) > 0)
+	{
+		switch (event.type)
+		{
+		case ENET_EVENT_TYPE_CONNECT:
+			printf ("A new client connected from %x:%u.\n", 
+					event.peer -> address.host,
+					event.peer -> address.port);
+			/* Store any relevant client information here. */
+			event.peer -> data = "Client information";
+			return 0;
+			break;
+		case ENET_EVENT_TYPE_RECEIVE:
+			printf ("A packet of length %u containing %s was received from %s on channel %u.\n",
+					event.packet -> dataLength,
+					event.packet -> data,
+					event.peer -> data,
+					event.channelID);
+			/* Clean up the packet now that we're done using it. */
+			enet_packet_destroy (event.packet);
+			return 1;
+			break;
+       
+		case ENET_EVENT_TYPE_DISCONNECT:
+			printf ("%s disconnected.\n", event.peer -> data);
+			/* Reset the peer's client information. */
+			event.peer -> data = NULL;
+			return 0;
+		}
+	}
 }
