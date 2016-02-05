de800fea4425590892baaafbabc8949b84f23414
Chris Walsh <chris24walsh@gmail.com>
2016-02-05 18:54:04 +0000

    Created Screen .cpp and .h files. Added press and release key methods to Screreen class. Each type of screen, e.g. menu or action, will inherit from Screen.

---
+++ allegroTest/allegroTest/Screen.h
@@ -0,0 +1,99 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <allegro5/allegro5.h>
+
+class Screen {
+public:
+	//Constructors
+	Screen();
+
+	//Press methods
+	press_up();
+	press_down();
+	press_left();
+	press_right();
+	press_enter();
+	press_space();
+	press_esc();
+	press_a();
+	press_b();
+	press_c();
+	press_d();
+	press_e();
+	press_f();
+	press_g();
+	press_h();
+	press_i();
+	press_j();
+	press_k();
+	press_l();
+	press_m();
+	press_n();
+	press_o();
+	press_p();
+	press_q();
+	press_r();
+	press_s();
+	press_t();
+	press_u();
+	press_v();
+	press_x();
+	press_y();
+	press_z();
+	press_0();
+	press_1();
+	press_2();
+	press_3();
+	press_4();
+	press_5();
+	press_6();
+	press_7();
+	press_8();
+	press_9();
+	//Release methods
+	release_up();
+	release_down();
+	release_left();
+	release_right();
+	release_enter();
+	release_space();
+	release_esc();
+	release_a();
+	release_b();
+	release_c();
+	release_d();
+	release_e();
+	release_f();
+	release_g();
+	release_h();
+	release_i();
+	release_j();
+	release_k();
+	release_l();
+	release_m();
+	release_n();
+	release_o();
+	release_p();
+	release_q();
+	release_r();
+	release_s();
+	release_t();
+	release_u();
+	release_v();
+	release_x();
+	release_y();
+	release_z();
+	release_0();
+	release_1();
+	release_2();
+	release_3();
+	release_4();
+	release_5();
+	release_6();
+	release_7();
+	release_8();
+	release_9();
+}
+
+#endif
