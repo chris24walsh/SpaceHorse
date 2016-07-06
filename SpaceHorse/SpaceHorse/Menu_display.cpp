226aba321c2778a46f526cd4b26a2b0a46a91828
christopher-ian-walsh <christopher.ian.walsh@hpe.com>
2016-07-06 10:16:51 +0100

    Added music for the docking station.

--- SpaceHorse/SpaceHorse/Menu_display.cpp
+++ SpaceHorse/SpaceHorse/Menu_display.cpp
@@ -6,8 +6,8 @@ Menu_display::Menu_display(int windowWidth, int windowHeight)
 	m_windowWidth(windowWidth),
 	m_windowHeight(windowHeight),
 	m_homeScreenOption(1), //'new game' option lit up by default
-	font(nullptr),
-	music(nullptr)
+	m_font(nullptr),
+	m_music(nullptr)
 {
 }
 
@@ -16,13 +16,13 @@ void Menu_display::load() //load all the resources for this display screen
 	if(!m_menuLoad)
 	{
 		//Load font
-		font = al_load_ttf_font("../../Font/pirulen.ttf",35,0);
-		if(!font) { menuFail("Could not load 'pirulen.ttf'.\n"); }
+		m_font = al_load_ttf_font("../../Font/pirulen.ttf",35,0);
+		if(!m_font) { menuFail("Could not load 'pirulen.ttf'.\n"); }
 
 		//Load audio
-		music = al_load_sample("../../sounds/intro-music.wav");
-		if (!music) { menuFail("Could not load 'space-ambient.wav'.\n"); }
-		al_play_sample(music, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
+		m_music = al_load_sample("../../sounds/intro-music.wav");
+		if (!m_music) { menuFail("Could not load 'intro-music.wav'.\n"); }
+		al_play_sample(m_music, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
 
 		m_menuLoad = true;
 	}
@@ -32,8 +32,8 @@ void Menu_display::unload() //free up all the resources for this display screen
 {
 	if(m_menuLoad)
 	{
-		if(font) { al_destroy_font(font); font = nullptr; }
-		if (music) { al_destroy_sample(music); music = nullptr;  }
+		if(m_font) { al_destroy_font(m_font); m_font = nullptr; }
+		if (m_music) { al_destroy_sample(m_music); m_music = nullptr;  }
 		
 		m_menuLoad = false;
 	}
@@ -62,9 +62,9 @@ void Menu_display::update()
 	}
 
 	//draw menu options
-	al_draw_text(font, al_map_rgb(c1R,c1G,c1B), m_windowWidth*0.5, m_windowHeight*0.3, ALLEGRO_ALIGN_CENTRE, "NEW GAME");
-	al_draw_text(font, al_map_rgb(c2R,c2G,c2B), m_windowWidth*0.5, m_windowHeight*0.5, ALLEGRO_ALIGN_CENTRE, "LOAD GAME");
-	al_draw_text(font, al_map_rgb(c3R,c3G,c3B), m_windowWidth*0.5, m_windowHeight*0.7, ALLEGRO_ALIGN_CENTRE, "QUIT");
+	al_draw_text(m_font, al_map_rgb(c1R,c1G,c1B), m_windowWidth*0.5, m_windowHeight*0.3, ALLEGRO_ALIGN_CENTRE, "NEW GAME");
+	al_draw_text(m_font, al_map_rgb(c2R,c2G,c2B), m_windowWidth*0.5, m_windowHeight*0.5, ALLEGRO_ALIGN_CENTRE, "LOAD GAME");
+	al_draw_text(m_font, al_map_rgb(c3R,c3G,c3B), m_windowWidth*0.5, m_windowHeight*0.7, ALLEGRO_ALIGN_CENTRE, "QUIT");
 }
 
 void Menu_display::menuFail(std::string failMessage)
@@ -80,8 +80,8 @@ Menu_display::~Menu_display(void)
 {
 	if(m_menuLoad)
 	{
-		if(font) { al_destroy_font(font); }
-		font = nullptr;
+		if(m_font) { al_destroy_font(m_font); }
+		m_font = nullptr;
 		m_menuLoad = false;
 	}
 }
