98730b9123dffcd578be88bb50c53b6eb34dbde2
zincsoda <steven.walsh39@gmail.com>
2016-07-10 15:21:25 +0100

    Added a space junk object

---
+++ SpaceHorse/SpaceHorse/Space_junk.cpp
@@ -0,0 +1,10 @@
+#include "Space_junk.h"
+
+SpaceJunk::SpaceJunk(int x, int y)
+	:x(x), y(y)
+{
+}
+
+SpaceJunk::~SpaceJunk(void) {
+
+}
---
+++ SpaceHorse/SpaceHorse/Space_junk.h
@@ -0,0 +1,15 @@
+#ifndef SPACE_JUNK_H
+#define SPACE_JUNK_H
+
+class SpaceJunk
+{
+private:
+	int x;
+	int y;
+
+public:
+	SpaceJunk(int x, int y);
+	~SpaceJunk();
+};
+
+#endif
